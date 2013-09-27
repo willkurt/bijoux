b60651b828a8148901d98c26660d57f493ad2bf2
Will Kurt <wckurt@gmail.com>
2013-09-26 17:01:55 -0700

    added scrabble words and scores, plus cleaned up a bit

--- main.r
+++ main.r
@@ -1,11 +1,21 @@
+
+
+
+srcdir <- function(file){
+  paste("./word_sources",file,sep="/")
+}
+outdir <- function(file){
+  paste("./results",file,sep="/")
+}
+
 #from http://www.r-bloggers.com/ascii-code-table-in-r/
 asc <- function(x) { strtoi(charToRaw(x),16L) }
 
 #freebsd/unix words
-words <- scan('./word_sources/words.txt',what="",sep="\n")
+words <- scan(srcdir("words.txt"),what="",sep="\n")
 
 #Official Scrabble words 'twl06'
-scrabble.words <- scan('./word_sources/twl06.txt',what="",sep="\n")
+scrabble.words <- scan(srcdir('twl06.txt'),what="",sep="\n")
 scrabble.words <- tolower(scrabble.words)
 
 in.alpha.order <- function(w,f=function(x){x}){
@@ -23,8 +33,8 @@ get.in.order.words <- function(ws,f=function(x){x}){
 ordered.words <- get.in.order.words(words)
 rev.ordered.words <- get.in.order.words(words,f=rev)
 
-write(ordered.words,file="ordered_words.txt")
-write(rev.ordered.words,file="rev_ordered_words.txt")
+write(ordered.words,file=outdir("ordered_words.txt"))
+write(rev.ordered.words,file=outdir("rev_ordered_words.txt"))
 
 #for the scrabble fans out there
 source('scrabble.r')
@@ -49,5 +59,5 @@ combine <- function(ws,s){
   paste(ws[or],s[or],sep=" : ",collapse="\n")
 }
 
-write(combine(scrabble.ordered.words,ordered.scores),file="scrabble_ordered.txt")
-write(combine(scrabble.rev.ordered.words,rev.ordered.scores),file="scrabble_rev_ordered.txt")
+write(combine(scrabble.ordered.words,ordered.scores),file=outdir("scrabble_ordered.txt"))
+write(combine(scrabble.rev.ordered.words,rev.ordered.scores),file=outdir("scrabble_rev_ordered.txt"))
